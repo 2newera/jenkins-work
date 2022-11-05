8b4f2072f876c7d6a5cfac4aee6ab68fbf333991
max <absj@gmail.com>
2022-11-05 21:38:14 +0530

    new commit from local v2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello World Jenkins mini project");  
+     System.out.println("Hello World Jenkins mini project status updated");  
     }  
 }  
